0531567e8f66831c3c5a2ab2116465d0b90d4638
arvi9 <adc9work@gmail.com>
2016-11-26 16:58:10 +0530

    Hello Word Program

--- HelloWord_project1/src/helloword/HelloWord.java
+++ HelloWord_project1/src/helloword/HelloWord.java
@@ -17,6 +17,7 @@ public class HelloWord {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello Java");
+        System.out.println("Hello");
     }
     
 }

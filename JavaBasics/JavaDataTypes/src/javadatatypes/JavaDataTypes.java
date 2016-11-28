be0c19fc89fd33d1a97058d0b520593da11c6431
arvi9 <adc9work@gmail.com>
2016-11-29 00:15:37 +0530

    Pre Increment and Post Increment

---
+++ JavaBasics/JavaDataTypes/src/javadatatypes/JavaDataTypes.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javadatatypes;
+
+/**
+ *
+ * @author invent
+ */
+public class JavaDataTypes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("--------------INTEGER DATA TYPE VALUE--------------------");
+        System.out.println("Byte.MAX_VALUE = "+Byte.MAX_VALUE);
+        System.out.println("Byte.MIN_VALUE = "+Byte.MIN_VALUE);
+        System.out.println("Short.MAX_VALUE = "+Short.MAX_VALUE);
+        System.out.println("Short.MIN_VALUE = "+Short.MIN_VALUE);
+        System.out.println("Integer.MAX_VALUE = "+Integer.MAX_VALUE);
+        System.out.println("Integer.MIN_VALUE = "+Integer.MIN_VALUE);
+        System.out.println("Long.MAX_VALUE = "+Long.MAX_VALUE);
+        System.out.println("Long.MIN_VALUE= "+Long.MIN_VALUE);
+        
+        System.out.println("------------------FLOTING DATA TYPE VALUE-----------------------");
+        System.out.println("Float.MAX_VALUE = "+Float.MAX_VALUE);
+         System.out.println("Float.MIN_VALUE = "+Float.MIN_VALUE);
+         System.out.println("Double.MAX_VALUE = "+ Double.MAX_VALUE);
+        System.out.println("Double.MIN_VALUE = "+ Double.MIN_VALUE);
+        
+        System.out.println("------------------CHAR DATA TYPE VALUE-----------------------");
+        System.out.println(Character.MAX_VALUE);
+        
+        
+    }
+    
+}

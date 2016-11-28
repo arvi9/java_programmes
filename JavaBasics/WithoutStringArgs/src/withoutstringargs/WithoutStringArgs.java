3554305598f8d8d66de7aefc9a6c2929c8eca182
arvi9 <adc9work@gmail.com>
2016-11-28 10:55:28 +0530

    Directory Changed

---
+++ JavaBasics/WithoutStringArgs/src/withoutstringargs/WithoutStringArgs.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package withoutstringargs;
+
+/**
+ *
+ * @author invent
+ */
+public class WithoutStringArgs {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // With out String[] args code compiles but JVM doesnt find any 
+    }
+    
+}
